f89a2207e81c05d50e3a549be70111ed0e0e06e4
Parth86 <60189466+Parth86@users.noreply.github.com>
2022-01-23 20:41:44 +0530

    refactored code

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ function calc(diff) {
         output[i] = Math.trunc(diff / notes[i])
         diff = diff % notes[i]
     }
-
     return output
 }
 
 
 btn.addEventListener('click', () => {
-    if (bill.value < 0) {
-        ('Error in the bill amount')
+    if(bill.value > 0){
+        if(cash.value > 0 && cash.value >= bill.value){
+            var diff = cash.value - bill.value
+            var notes = calc(diff)
+            for(var i=0; i<notes.length;i++){
+                noOfNotes[i].innerText = notes[i]
+            }
+        }else{
+            alert('Error in the cash amount')
+        }
+    }else{
+        alert('Error in the bill amount')
     }
-    if(cash.value < bill.value) {
-        alert('Error in the cash amount')
-    }
-    var diff = cash.value - bill.value
-    var notes = calc(diff)
-    for(var i=0; i<notes.length;i++){
-        noOfNotes[i].innerText = notes[i]
-    }
-
 })
